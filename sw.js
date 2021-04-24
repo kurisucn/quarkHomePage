4ab7a12ef993ae5d56b8bea915c11a9977de0c43
Kurium <65055020+kurisucn@users.noreply.github.com>
2021-04-24 02:54:31 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 importScripts('https://g.alicdn.com/kg/workbox/4.3.1/workbox-sw.js');
 // 缓存静态资源
 workbox.routing.registerRoute(
-    /([\/|.|\w|\s|-])*\.(?:png|gif|jpg|jpeg|webp|svg|ico|ttf|woff|js|woff2|eot|otf|css)/,
+    /([\/|.|\w|\s|-])*\.(?:png|gif|jpg|jpeg|webp|svg|ico|ttf|woff|woff2|eot|otf|js|css)/,
     new workbox.strategies.CacheFirst({
         cacheName: 'static-resources',
         plugins: [
             new workbox.expiration.Plugin({
-                maxEntries: 100, // 最大保存项目
-                maxAgeSeconds: 1 * 60 * 60, // 缓存 1 小时
+                maxEntries: 500, // 最大保存项目
+                maxAgeSeconds: 7 * 24 * 60 * 60, // 缓存 7 天
                 purgeOnQuotaError: true,
             }),
         ],
@@ -17,7 +17,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute(
     [
         {
-            revision: '158947',
+            revision: '158946',
             url: 'index.html',
         },
     ],
